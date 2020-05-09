11566241f5cc222f90c821dcd0125b11b6597b23
kuromon11 <yoshikikuro712@gmail.com>
2020-05-09 18:05:50 +0900

    create some components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="header">
-    ここはヘッダーです。
+  <div id=#app>
+    <div class="header">
+      <div class="headerLeft">
+        <h1 class="headerLeftTitle">ポートフォリオ</h1>
+      </div>
+      <div class="headerRight">
+        <a href="#" id="nav-about">About</a>
+        <a href="#" id="nav-works">Works</a>
+        <a href="#" id="nav-skill">Skill</a>
+        <a href="#" id="nav-contact">Contact</a>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped >
 .header{
   height: 100px;
   background-color: green;
   width: 100%;
+  display: flex;
+  &Left{
+    background-color: red;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faGlobe,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
+// import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
