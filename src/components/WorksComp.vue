ddd7800fb6797795e53dd47c8c5aee951830dd5a
kuromon11 <yoshikikuro712@gmail.com>
2020-05-10 13:33:05 +0900

    works component content furima create

--- src/components/WorksComp.vue
+++ src/components/WorksComp.vue
@@ -3,39 +3,57 @@
     <h1>Works</h1>
     <div class="works">
       <div class="worksContent">
-        <img src="../assets/logo.png" class="workContentImage">
-        <div class="workContentInfo">
-          <h2 class="workContentInfoTitle">Hurima</h2>
-          <p class="workContentInfoText">自分の行程に合わせた天気予報がわかるアプリ</p>
-          <p class="workContentInfoText">Language : Ruby・RubyonRails・AWS</p>
-          <p class="workContentInfoText">Language : Ruby・RubyonRails・AWS</p>
+        <img src="../assets/logo.png" class="worksContentImage">
+        <div class="worksContentInfo">
+          <h2 class="worksContentInfoTitle">Furima(フリマアプリ)</h2>
+          <router-link to="/"></router-link>
+          <p class="worksContentInfoText">テックキャンプの最終課題にて、5人チームでフリーマーケットのアプリケーションを作成しました。</p>
+          <p class="worksContentInfoText">
+            認証：<br>
+            ユーザー：tech<br>
+            パスワード：camp
+          </p>
+          <p class="worksContentInfoText">
+            出品者：<br>
+            メールアドレス：seller@seller<br>
+            パスワード：testtest
+          </p>
+          <p class="worksContentInfoText">
+            購入者：<br>
+            メールアドレス：buyer@buyer<br>
+            パスワード：testtest<br>
+            番号：4242424242424242<br>
+            期限：12月/20年<br>
+            セキュリティコード：123     
+          </p>          
+          <p class="worksContentInfoText">Language : Ruby・RubyonRails・AWS</p>
           <br>
-          <a href="/" class="workContentInfoSite">サイト</a>
-          <a href="/" class="workContentInfoGit">Github</a>
+          <a href="http://52.196.62.187" class="worksContentInfoSite">サイト</a>
+          <a href="https://github.com/kuromon11/freemarket_sample_71f" class="worksContentInfoGit">Github</a>
         </div>
       </div>
       
       <div class="worksContent">
-        <img src="../assets/logo.png" class="workContentImage">
-        <div class="workContentInfo">
-          <h2 class="workContentInfoTitle">Tavinow</h2>
-          <p class="workContentInfoText">自分の行程に合わせた天気予報がわかるアプリ</p>
-          <p class="workContentInfoText">Language : Ruby・RubyonRails・AWS</p>
+        <img src="../assets/logo.png" class="worksContentImage">
+        <div class="worksContentInfo">
+          <h2 class="worksContentInfoTitle">Tavinow</h2>
+          <p class="worksContentInfoText">自分の行程に合わせた天気予報がわかるアプリ</p>
+          <p class="worksContentInfoText">Language : Ruby・RubyonRails・AWS</p>
           <br>
-          <a href="/" class="card-link github-link">サイト</a>
-          <a href="/" class="card-link github-link">Github</a>
+          <a href="http://japan-traveler-blog.com/" class="worksContentInfoSite">サイト</a>
+          <a href="https://github.com/kuromon11/traveler_blog" class="worksContentInfoGit">Github</a>
         </div>
       </div>
 
       <div class="worksContent">
-        <img src="../assets/logo.png" class="workContentImage">
-        <div class="workContentInfo">
-          <h2 class="workContentInfoTitle">Tavinow</h2>
-          <p class="workContentInfoText">自分の行程に合わせた天気予報がわかるアプリ</p>
-          <p class="workContentInfoText">Language : Ruby・RubyonRails・AWS</p>
+        <img src="../assets/logo.png" class="worksContentImage">
+        <div class="worksContentInfo">
+          <h2 class="worksContentInfoTitle">Tavinow</h2>
+          <p class="worksContentInfoText">自分の行程に合わせた天気予報がわかるアプリ</p>
+          <p class="worksContentInfoText">Language : Ruby・RubyonRails・AWS</p>
           <br>
-          <a href="/" class="card-link github-link">サイト</a>
-          <a href="/" class="card-link github-link">Github</a>
+          <a href="http://52.196.62.187" class="worksContentInfoSite">サイト</a>
+          <a href="https://github.com/kuromon11/freemarket_sample_71f" class="worksContentInfoGit">Github</a>
         </div>
       </div>
     </div>
@@ -70,7 +88,35 @@ h1{
     width: 45%;
     margin: 1%;
     background-color: #f1f1f1;
-  border: 1px solid black;
+    border: 1px solid black;
+    &Info{
+      &Site{
+        border: 1px solid #00aaff;
+        text-decoration: none;
+        color: #00aaff;
+        margin: 0 10px;
+        padding: 5px 10px;
+        border-radius: 5px;
+      }
+      &Site:hover{
+        background-color: #00aaff;
+        color: white;
+      }
+      &Git{
+        border: 1px solid #00aaff;
+        text-decoration: none;
+        color: #00aaff;
+        margin: 0 10px;
+        padding: 5px 10px;
+        border-radius: 5px;
+        border-color: #171515;
+        color: #171515;
+      }
+      &Git:hover{
+        background-color: #171515;
+        color: white;
+      }
+    }
   }
 }
 </style>
